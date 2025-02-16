47fc1a7021536c124d987090702c1b1aca6c34f4
Fidachyo322 <baconvevo@mail.ru>
2025-02-16 18:05:48 +0300

    Add project files.

---
+++ Project01/test01.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//void test(int c) {
+//
+//	c++;
+//	cout << c << endl;
+//	return c;
+//}
+//
+//int main() {
+//	int c = 0;
+//	c = test(c);
+//	c = test(c);
+//	c = test(c);
+//	c = test(c);
+//	return 0;
+//}
